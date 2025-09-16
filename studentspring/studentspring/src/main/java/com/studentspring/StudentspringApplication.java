3db1fae9d1fd357caa851f17b120a0ed46f6d5be
Divakar@#321 <randhidivakar@gmail.com>
2025-09-16 21:55:45 +0530

    update pom12

--- studentspring/studentspring/src/main/java/com/studentspring/StudentspringApplication.java
+++ studentspring/studentspring/src/main/java/com/studentspring/StudentspringApplication.java
@@ -2,22 +2,11 @@ package com.studentspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/trial")
 public class StudentspringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentspringApplication.class, args);
 	}
-
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World";
-	}
-
 }
